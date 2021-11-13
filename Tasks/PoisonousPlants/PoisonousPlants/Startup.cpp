50123d4bd19a9a8c837129e85578cc0836f62d93
typhon04@gmail.com <!lekiti00drew>
2021-11-13 11:43:38 +0200

    Implemented performance test for various solutions

--- Tasks/PoisonousPlants/PoisonousPlants/Startup.cpp
+++ Tasks/PoisonousPlants/PoisonousPlants/Startup.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <ctime>
 
 #include "../PoisonousPlants/PoisonousPlants.cpp"
 
 using namespace std;
 
-int main()
+vector<int> GenerateSamplePlants() 
 {
     vector<int> plants;
 
@@ -16,7 +17,39 @@ int main()
     plants.push_back(4);
     plants.push_back(5);
 
-    int noFuneralDay = poisonousPlants(plants);
-
-    cout << noFuneralDay << endl;
+    return plants;
 }
+
+vector<int> GeneratePlants(int sequenceLenght)
+{
+	vector<int> plants;
+	int upperBound = 10;
+	int lowerBound = 1;
+
+	for (size_t i = 0; i < sequenceLenght; i++)
+	{
+		int n = ((rand() % (upperBound + 1 - lowerBound)) + lowerBound);
+		plants.push_back(n);
+	}
+
+	return plants;
+}
+
+int main()
+{
+	cout << "generate sequence..." << endl;
+	vector<int> plants = GeneratePlants(2000000);
+
+	cout << "start algo..." << endl;
+	time_t startTime = time(NULL);
+	//15 sec, day 21
+	//int noFuneralDay = poisonousPlantsQueue(plants);
+
+	//4 sec, day 21
+	int noFuneralDay = poisonousPlantsOwnQueue(plants);
+	time_t endTime = time(NULL);
+
+	cout << endTime - startTime << " seconds" << endl;
+	cout << noFuneralDay << endl;
+}
+
