6215091b5b23e0558e4a64bda3ce4e9d1db400c2
typhon04@gmail.com <!lekiti00drew>
2021-11-15 07:04:13 +0200

    Added pseudo code for nlogn solution

--- Tasks/PoisonousPlants/PoisonousPlants/PoisonousPlants.cpp
+++ Tasks/PoisonousPlants/PoisonousPlants/PoisonousPlants.cpp
@@ -92,6 +92,72 @@ inline int poisonousPlantsOwnQueue(vector<int> p)
     return currentDay;
 }
 
+inline int poisonousPlantsNLogN(vector<int> p)
+{
+    //v1
+
+    //kepp track of all alive
+
+    //list of dead from the last day
+    //list of dead from today
+
+    //fill list of dead on the first day
+
+    //while the newly dead is not empty
+        //for lenght of newly dead
+            //check element on the right of newly dead with the last alive on the left
+                //if not_dying -> do nothing
+                //if will die -> add to the dead on next day
+        //update newly dead and start new day of murder
+
+
+    //v2
+    //kepp track of all alive
+
+    //list of indexes of the dead from the last day
+    //list of indexes of the of dead from today
+
+    //fill list of dead on the first day
+
+    //while the newly dead is not empty
+        //for lenght of newly dead
+            //get next [newly dead]
+            //get [element right of [newly dead]]
+            //get [element last alive element left of right of newly dead]
+            //compare [element right of newly dead] with [element last alive element left of right of newly dead]
+                //if [right of newly dead] will die -> add to the dead_today list
+                //if not dying -> do nothing
+                
+        //kill all from dead_today
+        //replace newly_dead with dead_today 
+        //clear dead_today
+        //and start new day of murder
+
+
+    //v3 - WHICH DATA STRUCTURES TO USE
+
+    //kepp track of all alive -> vector
+    //list of indexes of the dead from the last day -> queue
+    //list of indexes of the of dead from today -> queue
+
+    //fill list of dead on the first day
+
+    //while the newly dead is not empty
+        //for lenght of newly dead
+            //get next [newly dead]
+            //get [element right of [newly dead]]
+            //get [element last alive element left of right of newly dead]
+            //compare [element right of newly dead] with [element last alive element left of right of newly dead]
+                //if [right of newly dead] will die -> add to the dead_today list
+                //if not dying -> do nothing
+
+        //kill all from dead_today -> HOW TO EFFICIENTLY DO IT
+        //replace newly_dead with dead_today 
+        //clear dead_today
+        //and start new day of murder
+
+}
+
 inline int poisonousPlants(vector<int> p)
 {
     return poisonousPlantsQueue(p);
