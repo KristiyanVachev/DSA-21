77832065815ab2079c61c7d0b6ab63cdf0196255
typhon04@gmail.com <!lekiti00drew>
2021-11-16 00:51:01 +0200

    added draft solution

--- Tasks/PoisonousPlants/PoisonousPlants/PoisonousPlants.cpp
+++ Tasks/PoisonousPlants/PoisonousPlants/PoisonousPlants.cpp
@@ -92,7 +92,7 @@ inline int poisonousPlantsOwnQueue(vector<int> p)
     return currentDay;
 }
 
-inline int poisonousPlantsNLogN(vector<int> p)
+inline int poisonousPlantsNLogN(vector<int> pesticideLevels)
 {
     //v1
 
@@ -138,24 +138,102 @@ inline int poisonousPlantsNLogN(vector<int> p)
 
     //kepp track of all alive -> vector
     //list of indexes of the dead from the last day -> queue
+    queue<int> yesterdayDead;
     //list of indexes of the of dead from today -> queue
+    queue<int> todayDead;
 
     //fill list of dead on the first day
+    int day = 1;
+    for (size_t i = 1; i < p.size(); i++)
+    {
+        if (pesticideLevels[i-1] < pesticideLevels[i])
+        {
+            yesterdayDead.push(i);
+        }
+    }
 
     //while the newly dead is not empty
-        //for lenght of newly dead
+    while (!yesterdayDead.empty())
+    {
+        day++;
+        int yesterdayDeathsCount = yesterdayDead.size();
+
+        for (size_t i = 0; i < yesterdayDeathsCount; i++)
+        {
             //get next [newly dead]
+            int newlyDeadIndex = yesterdayDead.front();
+            yesterdayDead.pop();
             //get [element right of [newly dead]]
+            //TODO: Check if out of array
+            int rightOfNewlyDeadIndex = pesticideLevels[newlyDeadIndex + 1];
+
             //get [element last alive element left of right of newly dead]
+            int leftOfNewlyDeadIndex = -1;
+            for (size_t i = newlyDeadIndex - 1; i >= 0; i--)
+            {
+                if (pesticideLevels[i] != -1) //Hack to keep using a queue
+                {
+                    leftOfNewlyDeadIndex = i;
+                    break;
+                }
+            }
+
             //compare [element right of newly dead] with [element last alive element left of right of newly dead]
+            if (leftOfNewlyDeadIndex > 0 && pesticideLevels[leftOfNewlyDeadIndex] < pesticideLevels[rightOfNewlyDeadIndex])
+            {
                 //if [right of newly dead] will die -> add to the dead_today list
                 //if not dying -> do nothing
+                todayDead.push(rightOfNewlyDeadIndex);
+                pesticideLevels[newlyDeadIndex] = -1;
+            }
+        }
 
-        //kill all from dead_today -> HOW TO EFFICIENTLY DO IT
-        //replace newly_dead with dead_today 
-        //clear dead_today
-        //and start new day of murder
+        //TODO make sure yesterday_dead are all gone
 
+        //kill all from yesterday_dead from list_of_alive
+        //add new indexes to newly_dead
+        //clear dead_today for the next day of murder 
+        int shiftLeftBecauseOfDeadCount = 0;
+
+        int i = 0;
+        while (true)
+        {
+            int nextTodayDeathIndex = todayDead.front();
+
+            if (pesticideLevels[i] == -1)
+            {
+                shiftLeftBecauseOfDeadCount++;
+                pesticideLevels.erase(i); //WTF?
+            }
+
+            if (nextTodayDeathIndex - shiftLeftBecauseOfDeadCount == i)
+            {
+                yesterdayDead.push(nextTodayDeathIndex - shiftLeftBecauseOfDeadCount);
+                todayDead.pop();
+            }
+        }
+
+        //for (size_t i = 0; i < pesticideLevels.size(); i++) //TODO chance to while
+        //{
+        //    int nextTodayDeathIndex = todayDead.front();
+
+        //    if (pesticideLevels[i] == -1)
+        //    {
+        //        shiftLeftBecauseOfDeadCount++;
+        //        pesticideLevels.erase(i); //WTF?
+        //    }
+
+        //    if (nextTodayDeathIndex - shiftLeftBecauseOfDeadCount == i)
+        //    {
+        //        yesterdayDead.push(nextTodayDeathIndex - shiftLeftBecauseOfDeadCount);
+        //        todayDead.pop();
+        //    }
+        //}
+
+        //TODO make sure todayDead is empty.
+    }
+
+    return day - 1;
 }
 
 inline int poisonousPlants(vector<int> p)
